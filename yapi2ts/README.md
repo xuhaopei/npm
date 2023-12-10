582573045f3ca6b04052750e8ba571ed107f32c1
inke_xuhapei <xuhaopei@inke.cn>
2023-12-10 17:20:53 +0800

    feat: 添加yapi代码

---
+++ yapi2ts/README.md
@@ -0,0 +1,26 @@
+# 功能介绍
+通过传入yapi的url，可以打印出对应的ts接口信息。
+
+# 安装
+npm i yapi2ts -g 
+
+# 使用
+yapi2ts
+
+# 开发人员维护：
+
+clone 项目后，进入项目的根目录下，执行打包 
+
+```sh
+pnpm i 
+
+// 打包
+npm run build
+
+// 本地包链接成全局包
+npm link
+
+// 完成调试
+npm unlink
+```
+
---
+++ yapi2ts/src/utils/BaseApiYapi.ts
@@ -0,0 +1,34 @@
+/**
+ * YApi平台的各个api调用
+ */
+// 引入别名路径包，因为tsc不支持编译别名路径，故此需要这个包进行路径转换。
+
+import log from "@/utils/BaseLog";
+import axios from "axios";
+const instance = axios.create({
+    baseURL: 'https://yapi.inkept.cn/',
+    timeout: 5000,
+});
+instance.interceptors.response.use((res) => {
+    return res;
+}, (error) => {
+    log.error('接口权限有问题！请检查当前token是否有访问该接口的权限！')
+    return Promise.reject(error);
+})
+// 获取某个分类下接口列表 https://hellosean1025.github.io/yapi/openapi.html
+export function interfaceListCat(params: YapiQueryListCat): Promise<YapiResponseListCat> {
+    return instance.request({
+        url: '/api/interface/list_cat',
+        method: 'GET', // default
+        params
+    })
+}
+
+// 获取接口数据（有详细接口数据定义文档） https://hellosean1025.github.io/yapi/openapi.html
+export function interfaceGet(params: YapiQueryGet): Promise<YapiResponseGet> {
+    return instance.request({
+        url: '/api/interface/get',
+        method: 'GET', // default
+        params
+    })
+}
