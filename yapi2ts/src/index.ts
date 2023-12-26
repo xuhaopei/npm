9429a47a093bf7be2336859d5cea9a903bbfa885
opao <xuhaopei@inke.cn>
2023-12-26 14:58:28 +0800

    feat

--- yapi2ts/src/index.ts
+++ yapi2ts/src/index.ts
@@ -5,14 +5,14 @@ const program = require('commander'); // 处理用户命令
 program
     .version('1.0.0')
     .command('render')
-    .description('以某个模板初始化一个新工程')
+    .description('渲染api')
     .action((name: string) => {
         renderApi()
     })
 program
     .version('1.0.0')
     .command('del')
-    .description('以某个模板初始化一个新工程')
+    .description('删除token文件')
     .action((name: string) => {
         delToken()
     })
